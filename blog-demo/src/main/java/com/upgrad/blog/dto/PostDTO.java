0a3711e9ecb19b1de58df3d8a55c19fc2691ada6
Shivam <singhal29797shivam@gmail.com>
2020-06-01 03:53:44 +0530

    Start Commit

---
+++ blog-demo/src/main/java/com/upgrad/blog/dto/PostDTO.java
@@ -0,0 +1,50 @@
+package com.upgrad.blog.dto;
+
+/**
+ * TODO: 2.1. Declare 6 private instance variables in this class named as postId,
+ * emailId, tag, title, description, timestamp. Out of these 6 variables, postId
+ * will be of type int and timestamp will be of type LocalDateTime
+ * (import java.time.LocalDateTime). Other four variables will be of type String.
+ *
+ * TODO: 2.2. Provide getters and setters for each of the instance variables.
+ *
+ * Note: Uncomment the toString() method given below, instead of writing a new one.
+ */
+//*****************START*****************Solution 2.1 + 2.2*****************
+
+//*****************END*****************Solution 2.1 + 2.2*****************
+public class PostDTO {
+
+	//*****************START*****************Solution 2.1 + 2.2*****************
+
+    //*****************END*****************Solution 2.1 + 2.2*****************
+//    Uncomment the toString() method below.
+//    @Override
+//    public String toString() {
+//        return "PostDTO{" +
+//                "postId=" + postId +
+//                ", emailId='" + emailId + '\'' +
+//                ", tag='" + tag + '\'' +
+//                ", title='" + title + '\'' +
+//                ", description='" + description + '\'' +
+//                ", timestamp=" + timestamp +
+//                '}';
+//    }
+    
+//    public static void main(String[] args) {
+//		PostDTO postDTO = new PostDTO();
+//		postDTO.setPostId(1);
+//		postDTO.setEmailId("dummy@dummy.com");
+//		postDTO.setTag("tag");
+//		postDTO.setTitle("title");
+//		postDTO.setDescription("Description");
+//		postDTO.setTimestamp(LocalDateTime.now());
+//		
+//		System.out.println(postDTO);
+//		
+//		/**
+//		 * Your output should be similar to this.
+//		 * PostDTO{postId=1, emailId='dummy@dummy.com', tag='tag', title='title', description='Description', timestamp=2020-05-22T21:01:34.483363800}
+//		 */
+//	}
+}
